22e8bb4a34b5156c8064e8e426e979c91e344ec2
root <root@ip-172-31-27-116.ec2.internal>
2024-04-06 05:01:47 +0000

    sample.java

---
+++ sample.java
@@ -0,0 +1,10 @@
+Certainly! Here’s a simple Java “Hello, World!” program to get you started:
+
+Java
+
+// Your First Program
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
